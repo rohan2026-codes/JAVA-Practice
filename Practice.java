a4af7374dd9380256bb7d5325fea97ff07053811
Rohan Sharma <rohan22006@gmail.com>
2026-07-30 07:06:29 +0530

    Practice.java modified

--- Practice.java
+++ Practice.java
@@ -3,29 +3,37 @@ public class Practice{
     public static void main(String[] args){
 
         //printing out data types
-        /*String a="Rohan Sharma";
+        String a="Rohan Sharma";
+        byte b=100;
+        short c=3000;
         int age=19;
+        long d=1000000;
+        float e=3.14f;
         double cgpa=7.5;
         boolean pass=true;
-        char b='a';
+        char h='a';
         System.out.println("Perfect");
-        System.out.println(age +" is my age");
-        System.out.println(a + " is my name");
-        System.out.println(cgpa + "is my cgpa");
-        System.out.println(pass + " is my result");
-        System.out.println(b + " is a character");*/
+        System.out.println(age +" is integer");
+        System.out.println(a + " is string");
+        System.out.println(cgpa + "is double");
+        System.out.println(pass + " is bool");
+        System.out.println(b + " is byte");
+        System.out.println(c+"is short");
+        System.out.println(d+"is long");
+        System.out.println(e+"is float");
+        System.out.println(h+"is char");
         
         //taking in input
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter name: ");
         String name=sc.nextLine();
         System.out.println("Hello " + name);
-        System.out.print("enter int: ");
-        int a=sc.nextInt();
+        System.out.print("enter age: ");
+        int f=sc.nextInt();
+        System.out.println(f+" age ");
         System.out.print("enter decimal: ");
-        double b=sc.nextDouble();
-        System.out.println(a);
-        System.out.println(b);
+        double g=sc.nextDouble();
+        System.out.println(g+" decimal ");
         sc.close();
     }
 }
